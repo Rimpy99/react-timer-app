5db4c5b5af45f85647eea11d476d656e2c416492
Rimpy99 <70006pawel@gmail.com>
2022-08-27 18:10:41 +0200

    App and Timer components prepared + simple styles created

--- src/App.js
+++ src/App.js
@@ -1,12 +1,21 @@
-import './App.css';
+import styled from 'styled-components';
+
 import Timer from './components/Timer';
 
-function App() {
+const App = () => {
   return (
-    <div className="App">
+    <AppContainer>
       <Timer/>
-    </div>
+    </AppContainer>
   );
 }
+
+const AppContainer = styled.div`
+  width: 100vw;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
 
 export default App;
--- src/components/Timer.js
+++ src/components/Timer.js
@@ -1,9 +1,30 @@
+import styled from 'styled-components';
 
 const Timer = () => {
 
     return(
-        <>
-            yo
-        </>
+        <Container>
+            <TimerContainer>t</TimerContainer>
+            <ButtonsContainer>t</ButtonsContainer>
+        </Container>
     )
 }
+
+const Container = styled.div`
+    width: 500px;
+    height: 500px;
+`;
+
+const TimerContainer = styled.div`
+    width: 100%;
+    height: 70%;
+    background-color: green;
+`;
+
+const ButtonsContainer = styled.div`
+    width: 100%;
+    height: 30%;
+    background-color: blue;
+`;
+
+export default Timer;
