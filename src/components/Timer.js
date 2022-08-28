2a94011281e8948fd715c040b5d3b5d5623b8ad1
Rimpy99 <70006pawel@gmail.com>
2022-08-28 12:01:01 +0200

    changeButtonCondition function created in Timer component

--- src/components/Timer.js
+++ src/components/Timer.js
@@ -1,16 +1,42 @@
+//STYLED-COMPONENTS IMPORTS
 import styled from 'styled-components';
 
+//REDUX IMPORTS
+import { useSelector, useDispatch } from 'react-redux';
+// import { minIncrement, minReset } from '../features/minTimer/minTimerSlice';
+// import { secIncrement, secReset } from '../features/secTimer/secTimerSlice';
+import { changeToTrue, changeToFalse } from '../features/buttonCondition/buttonConditionSlice';
+
+
 const Timer = () => {
+    const currentButtonCondition = useSelector((state) => state.buttonCondition.value);
+
+    const dispatch = useDispatch();
+
+    const changeButtonCondition = () => {
+        if (currentButtonCondition === true){
+            dispatch(changeToFalse());
+        }else if(currentButtonCondition === false){
+            dispatch(changeToTrue());
+        }
+    }
 
     return(
         <Container>
             <TimerContainer>t</TimerContainer>
             <ButtonsContainer>
-                <button>START</button>
+                <button 
+                    onClick={() => changeButtonCondition()}
+                >
+                    {currentButtonCondition ? 'START' : 'STOP'}
+                </button>
             </ButtonsContainer>
         </Container>
     )
 }
+
+
+//STYLED-COMPONENTS STYLES
 
 const Container = styled.div`
     width: 500px;
